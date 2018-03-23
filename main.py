c3e804a79c6e2da457ddfb564c0953dc92f7f8da
Dean P. Ottewell <dean.p.ottewell@gmail.com>
2018-03-23 13:14:50 +0000

    added parser

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 """
 
-Parsing compoennts to extract character
-specific messages.
+Parsing compoennts to extract character specific message responses.
+
+Takes dummy approach that all messages said before characater is directed to the character
 
 """
 import csv
 import re
+import argparse
+
 
 FILE_PATH = "All-seasons.csv"
 MAIN_CHAR = "Stan"
@@ -26,14 +29,22 @@ def csv_parser(obj):
             main_msg_res.append( (prev_row[3],main_res) )
             
             main_res = ''
-        elif re.match(row[2], MAIN_CHAR):
+        elif re.match(MAIN_CHAR, row[2]):
             main_res += row[3][:-1]
         else:
             prev_row = row
 
-
+    print(main_msg_res)
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("character", help="Main name of character to gather all responses from. The name is case insensitive.")
+    args = parser.parse_args()
+    print(args.character)
+
+    MAIN_CHAR = '(?i)' + args.character 
+
     with open(FILE_PATH, "r") as o:
-        print("Parsing conversations")
+        print("Processing conversations: ", MAIN_CHAR)
         csv_parser(o)
