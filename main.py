8a9dd347b0304871584b962c5d1ad3d32af9a4af
Dean P. Ottewell <dean.p.ottewell@gmail.com>
2018-03-23 13:19:17 +0000

    just cleaned func

--- main.py
+++ main.py
@@ -11,11 +11,10 @@ import argparse
 
 
 FILE_PATH = "All-seasons.csv"
-MAIN_CHAR = "Stan"
 
 main_msg_res = []
 
-def csv_parser(obj):
+def csv_convo_parser(obj, name):
     reader = csv.reader(obj)
     
     main_res = ''
@@ -24,12 +23,12 @@ def csv_parser(obj):
     for row in reader:
         if main_res != '':
             print(prev_row[2], "---", prev_row[3][:-1])
-            print(MAIN_CHAR, ":---", main_res)
+            print(name, ":---", main_res)
 
             main_msg_res.append( (prev_row[3],main_res) )
             
             main_res = ''
-        elif re.match(MAIN_CHAR, row[2]):
+        elif re.match(name, row[2]):
             main_res += row[3][:-1]
         else:
             prev_row = row
@@ -43,8 +42,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print(args.character)
 
-    MAIN_CHAR = '(?i)' + args.character 
+    name = '(?i)' + args.character 
 
     with open(FILE_PATH, "r") as o:
-        print("Processing conversations: ", MAIN_CHAR)
-        csv_parser(o)
+        print("Processing conversations: ", name)
+        csv_convo_parser(o, name)
